a3dc4431e0e23413a2b03fee0f2e45bb603f6b6e
IcePear-Jzx <icepear.jzx@gmail.com>
2020-06-24 17:46:42 +0800

    finish KNN

--- Lab2/supervise/src/KNN.py
+++ Lab2/supervise/src/KNN.py
@@ -3,28 +3,24 @@ import numpy as np
 
 class KNN:
 
-    def __init__(self, k=10):
-        self.k = k
+    def __init__(self, args):
+        self.k = args.k
         self.x_train = None
         self.y_train = None
 
     def fit(self, x_train, y_train):
         self.x_train = x_train
         self.y_train = y_train
-        return self
     
     def predict(self, x_test):
-        y_test = [self._predict(x) for x in x_test]
-        return np.array(y_test)
+        y_pred = []
+        for attr_test in x_test:
+            distances = [self.euclidean_distance(attr_test, attr_train) for attr_train in self.x_train]
+            top_k_index = np.argsort(distances)[:self.k]
+            top_k_value = self.y_train[top_k_index]
+            values, counts = np.unique(top_k_value, return_counts=True)
+            y_pred.append(values[np.argmax(counts)])
+        return np.array(y_pred)
     
-    def _predict(self, x):
-        distances = [np.sqrt(np.sum((x_train - x)**2)) for x_train in self.x_train]
-        nearest = np.argsort(distances)[:self.k]
-        top_k = [self.y_train[index] for index in nearest]
-        d = {}
-        for i in top_k:
-            if i not in d:
-                d[i] = 0
-            else:
-                d[i] += 1
-        return sorted(d.items(), key = lambda kv:(kv[1], kv[0]), reverse=True)[0][0]
+    def euclidean_distance(self, x1, x2):
+        return np.sqrt(np.sum(np.power(x1 - x2, 2)))
--- Lab2/supervise/src/main.py
+++ Lab2/supervise/src/main.py
@@ -1,82 +1,102 @@
+import argparse
 import numpy as np
 import csv
 from sklearn.preprocessing import LabelEncoder
 from KNN import KNN
-from LogisticRegression import LogisticRegression as LR
+from LR import LR
 from SVM import SVM
 
 
-MAT_DATASET = '../data/student/student-mat.csv'
-POR_DATASET = '../data/student/student-por.csv'
+DATA_PATH_MAP = {
+    'MAT': '../data/student-mat.csv',
+    'POR': '../data/student-por.csv',
+}
+
+MODEL_MAP = {
+    'KNN': KNN,
+    'SVM': SVM,
+    'LR': LR,
+}
+
+STR_COLS = [0, 1, 3, 4, 5, 8, 9, 10, 11, 15, 16, 17, 18, 19, 20, 21, 22]
+NUM_COLS = [2, 6, 7, 12, 13, 14, 23, 24, 25, 26, 27, 28, 29, 30, 31]
 
 
-def load_data(filename=MAT_DATASET, algorithm='KNN', use_G = True):
-    labels = []
-    attributes = [[] for i in range(32)]
-    with open(filename, 'r') as f:
+def load_data(data_path, model, useG):
+    y = []
+    x = [[] for _ in range(32)]
+    with open(data_path, 'r') as f:
         lines = csv.reader(f, delimiter=';')
-        next(lines) # first line
+        lines.__next__()
         for line in lines:
-            labels.append(line[-1])
+            y.append(line[-1])
             for i in range(32):
-                attributes[i].append(line[i])
-    if algorithm == 'SVM':
-        labels = list(map(lambda x: 1 if int(x) >= 10 else -1, labels))
+                x[i].append(line[i])
+    
+    if model == 'SVM':
+        y = [1 if int(row) >= 10 else -1 for row in y]
     else:
-        labels = list(map(lambda x: 1 if int(x) >= 10 else 0, labels))
-    for i in [0, 1, 3, 4, 5, 8, 9, 10, 11, 15, 16, 17, 18, 19, 20, 21, 22]:
-        attributes[i] = LabelEncoder().fit_transform(attributes[i])
-    for i in range(len(attributes)):
-        attributes[i] = list(map(lambda x: int(x), attributes[i]))
-    x = np.array(attributes).T
-    if not use_G:
-        x = x[:, :-2]
-    return x, labels
+        y = [1 if int(row) >= 10 else 0 for row in y]
 
+    for i in NUM_COLS:
+        x[i] = [int(ele) for ele in x[i]]
+    for i in STR_COLS:
+        x[i] = LabelEncoder().fit_transform(x[i])
 
-def train_test_split(x, y, test_size=0.3):
-    # shuffle
-    x = np.array(x)
     y = np.array(y).reshape(-1, 1)
+    x = np.array(x).T
+
+    if not useG:
+        x = x[:, :-2]
+
+    return x, y
+
+
+def split_data(x, y, train_size=0.7):
     data = np.concatenate((x, y), axis=-1)
     np.random.shuffle(data)
     x = data[:, :-1]
-    y = data[:,-1]
+    y = data[:, -1]
 
-    # split
-    train_num = int(x.shape[0]*(1-test_size))
-    x_train = np.array(x[:train_num])
-    y_train = np.array(y[:train_num])
-    x_test = np.array(x[train_num:])
-    y_test = np.array(y[train_num:])
+    train_num = int(x.shape[0] * train_size)
+    x_train = x[:train_num]
+    y_train = y[:train_num]
+    x_test = x[train_num:]
+    y_test = y[train_num:]
+    
     return x_train, y_train, x_test, y_test
 
 
 def evaluate(y_true, y_pred):
     TP = FP = FN = TN = 0
     for i in range(len(y_true)):
-        if y_true[i] == 1:
-            if y_pred[i] == 1:
-                TP += 1
-            else:
-                FN += 1
+        if y_true[i] == 1 and y_pred[i] == 1:
+            TP += 1
+        elif y_true[i] == 1 and y_pred[i] == 0:
+            FN += 1
+        elif y_true[i] == 0 and y_pred[i] == 1:
+            FP += 1
         else:
-            if y_pred[i] == 1:
-                FP += 1
-            else:
-                TN += 1
+            TN += 1
     P = TP / (TP + FP)
     R = TP / (TP + FN)
-    return P, R, 2 * P * R / (P + R)
+    F = 2 * P * R / (P + R)
+    return P, R, F
 
 
 if __name__ == '__main__':
-    x, y = load_data(POR_DATASET, algorithm='SVM')
-    x_train, y_train, x_test, y_test = train_test_split(x, y)
-    # model = KNN()
-    # model = LR()
-    model = SVM(kernel='rbf')
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, choices=['KNN', 'SVM', 'LR'], default='KNN')
+    parser.add_argument('--kernel', type=str, choices=['LINEAR', 'RBF'], default='LINEAR')
+    parser.add_argument('--dataset', type=str, choices=['MAT', 'POR'], default='MAT')
+    parser.add_argument('--useG', type=bool, default=False)
+    parser.add_argument('--k', type=int, default=10)
+    args = parser.parse_args()
+
+    x, y = load_data(DATA_PATH_MAP[args.dataset], args.model, args.useG)
+    x_train, y_train, x_test, y_test = split_data(x, y)
+    model = MODEL_MAP[args.model](args)
     model.fit(x_train, y_train)
     y_pred = model.predict(x_test) 
-    # print(y_pred)
     print(evaluate(y_test, y_pred))
